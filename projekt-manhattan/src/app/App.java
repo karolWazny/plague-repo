7ae91e733826d41964d8fc23b3a6958be8b5c768
https://github.com/kuba342 <j.derda010@gmail.com>
2020-04-17 13:43:26 +0200

    Poprawa w main

--- projekt-manhattan/src/app/App.java
+++ projekt-manhattan/src/app/App.java
@@ -1,11 +1,13 @@
 package app;
 
-import map.Byt;
+//import map.Byt;
 import map.Mapa;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Mapa map = new Mapa(10);
-        map.wyswietl_Mape();
+        Mapa map = new Mapa(10,20);
+        map.wyswietlMape();
+
+        System.out.println(map);
     }
 }
