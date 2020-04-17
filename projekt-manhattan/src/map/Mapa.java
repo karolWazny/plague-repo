a0a172fa0d5f7d060ecc5c02fe2c818d99fa9334
Karol Wazny <karoljanwazny@gmail.com>
2020-04-17 13:20:57 +0200

    dodanie dwoch wymiarow mapy

--- projekt-manhattan/src/map/Mapa.java
+++ projekt-manhattan/src/map/Mapa.java
@@ -2,31 +2,32 @@ package map;
 
 public class Mapa {
     //Pole identyfikujące rozmiar mapy - ilość pól tekstowych w konsoli
-    private int rozmiar;     
+    private int dlugosc;
+    private int szerokosc;     
     
     //Pole będące tablicą obiektów typu Byt
     public Byt[][] tablica;
 
     //Konstruktor mapy
-    public Mapa(int rozmiar){
-        this.rozmiar = rozmiar;
-    
-        tablica = new Byt[this.rozmiar][this.rozmiar];
+    public Mapa(int dlugosc, int szerokosc){
+        this.dlugosc = dlugosc;
+        this.szerokosc = szerokosc;
+        tablica = new Byt[this.dlugosc][this.szerokosc];
 
-        for(int i=0; i<this.rozmiar; i++){
-            for(int j=0; j<this.rozmiar; j++){
+        for(int i=0; i<this.dlugosc; i++){
+            for(int j=0; j<this.szerokosc; j++){
                 this.tablica[i][j] = new Byt("0");
             }
         }
     }
-
+    @Override
     public String toString(){
-        return "Mapa o rozmiarze: " + this.rozmiar + " na " + this.rozmiar;
+        return "Mapa o rozmiarze: " + this.dlugosc + " na " + this.szerokosc;
     }
 
-    public void wyswietl_Mape(){
-        for(int i=0; i<this.rozmiar; i++){
-            for(int j=0; j<this.rozmiar; j++){
+    public void wyswietlMape(){
+        for(int i=0; i<this.dlugosc; i++){
+            for(int j=0; j<this.szerokosc; j++){
                 System.out.print(this.tablica[i][j]);
             }
             System.out.print("\n");
