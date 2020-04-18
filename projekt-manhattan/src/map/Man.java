0c578dd24f1f991c88f4b778073370384fcb2210
Karol Wazny <karoljanwazny@gmail.com>
2020-04-19 01:04:54 +0200

    nowe interfejsy

--- projekt-manhattan/src/map/Man.java
+++ projekt-manhattan/src/map/Man.java
@@ -1,8 +1,17 @@
 package map;
 
-public class Man extends Being {
+public class Man extends Being implements IMovable, IDiseaseSensitive {
     public Man(String id)
     {
         super(id, 'm');
     }
+    @Override
+    public void move() {
+
+    }
+    @Override
+    public void performIllness() {
+        
+    }
+
 }
