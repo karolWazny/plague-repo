98dc1ae4d268d62a99b49517a90f1fbcdbdfc5df
Karol Wazny <karoljanwazny@gmail.com>
2020-04-30 18:07:43 +0200

    Zmiana Mapable na IRecord

---
+++ projekt-manhattan/src/map/IRecord.java
@@ -0,0 +1,10 @@
+package map;
+
+public interface IRecord {
+    public void move();
+    public Coordinates getVerHor();
+    @Deprecated
+    public void setVerHor(int[] newVerHor);
+    public void setVerHor(Coordinates currentVerHor);
+    public void infectNeighbours(Map map);
+}
