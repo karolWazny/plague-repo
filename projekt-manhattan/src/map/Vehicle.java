34e043dc59e5c121e6dafb2f096eb9cc6ee4c8e6
https://github.com/kuba342 <j.derda010@gmail.com>
2020-05-01 11:40:16 +0200

    Klasa vehicle

--- projekt-manhattan/src/map/Vehicle.java
+++ projekt-manhattan/src/map/Vehicle.java
@@ -1,13 +1,18 @@
 package map;
 
-public abstract class Vehicle extends Being{
+import java.util.*;
+
+public abstract class Vehicle extends Being implements IMovable{
     private int capacity;
     private int velocity;
     private static int numVeh = 0;
     
+    private List<IMovable> passengers; //Kontener na obiekty
+    private int numPassengers = 0;
     //Konstruktory
     public Vehicle(){
         super("Vehicle",'V');
+        passengers = new ArrayList<>();
         numVeh++;
     }
 
@@ -15,6 +20,7 @@ public abstract class Vehicle extends Being{
         super(id, representation);
         this.capacity = capacity;
         this.velocity = velocity;
+        passengers = new ArrayList<>();
         numVeh++;
     }
 
@@ -22,6 +28,14 @@ public abstract class Vehicle extends Being{
     public String toString(){
         return "Vehicle nr " + numVeh + " with capacity " + this.capacity + 
         " and velocity " + this.velocity;
+    }
+
+    public Coordinates move(){
+        Coordinates newPosition = new Coordinates(0,0);
+        //Tutaj będzie dużo pierdzielenia z tym ruchem...
+        //Bo naszym celem będzie dojechać do szpitala
+        //I w jakiś sposób musimy sygnalizować człowiekiem, że tam ma karetka dojechać
+        return newPosition;
     }
 
     public int getVelocity(){
@@ -34,5 +48,19 @@ public abstract class Vehicle extends Being{
 
     public int getNumVeh(){
         return numVeh;
+    }
+
+    public List<IMovable> getPassengers(){
+        return passengers;
+    }
+
+    //Dodaję pasażera do pojazdu, jeśli nie ma miejsca, nie robię tego
+    public void setPassenger(IMovable patient){
+        if(this.numPassengers<this.capacity){
+            this.passengers.add(patient);
+        }
+        else{
+            return;
+        }   
     }
 }
