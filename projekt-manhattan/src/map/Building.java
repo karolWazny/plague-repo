616aaf2e135652a5909c868a524b9f63b207b37a
John Doe <konrad.bieganowski@o2.pl>
2020-05-03 15:51:29 +0200

    Poprawki w Building i Hospital

--- projekt-manhattan/src/map/Building.java
+++ projekt-manhattan/src/map/Building.java
@@ -2,13 +2,31 @@ package map;
 
 public abstract class Building extends Being{
     
+    //Pola
     private static int blockCounter = 0;
+    private int capacity;
     
+    //Konstruktor
     public Building(){
         super("Block "+ blockCounter, 'B');
+        blockCounter++;
     }
-    public Building(char representation){ //Hospital
-        super("Block "+ blockCounter, 'H');
+    public Building(String id, char representation, int capacity){ //Hospital
+        super(id, representation);
+        this.capacity = capacity;
+        blockCounter++;
+    }
+
+    //Metody
+    public int getCapacity(){
+        return capacity;
     }
     
+    public int getblockCounter(){
+        return blockCounter;
+    }
+
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
 }
