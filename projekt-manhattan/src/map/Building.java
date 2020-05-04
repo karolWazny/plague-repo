794ab1cb1e995323144d765d4b01d1e25e62775a
https://github.com/kuba342 <j.derda010@gmail.com>
2020-05-04 18:56:48 +0200

    Building i Hospital

--- projekt-manhattan/src/map/Building.java
+++ projekt-manhattan/src/map/Building.java
@@ -2,16 +2,33 @@ package map;
 
 public abstract class Building extends Being{
     private static int blockCounter = 0;
+    private int capacity;
     
     ////////////////////////////
 
     public Building(){
         super("Block "+ blockCounter, 'B');
+        blockCounter++;
     }
-    public Building(char representation){ //Hospital
-        super("Block "+ blockCounter, 'H');
+    public Building(String id, char representation, int capacity){ //Hospital
+        super(id, representation);
+        this.capacity = capacity;
+        blockCounter++;
+    }
+
+    ////////////////////////////
+    
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
     }
     
     ////////////////////////////
     
+    public int getCapacity(){
+        return capacity;
+    }
+    
+    public int getblockCounter(){
+        return blockCounter;
+    }
 }
--- projekt-manhattan/src/map/Hospital.java
+++ projekt-manhattan/src/map/Hospital.java
@@ -1,10 +1,25 @@
 package map;
 
 public class Hospital extends Building {
-    
+    private static int hospitalCounter = 0;
+
+    ////////////////////////////
+    //Konstruktor
+    public Hospital(){
+        super("Hospital", 'H', 100);
+        hospitalCounter++;
+    }
+
+    public Hospital(String id, char representation, int capacity){
+        super(id, representation, capacity);
+        hospitalCounter++;
+    }
+
     ////////////////////////////
 
-    public Hospital(){
-        super('H');
+    ////////////////////////////
+    //Metody
+    public int getHospitalCounter(){
+        return hospitalCounter;
     }
 }
