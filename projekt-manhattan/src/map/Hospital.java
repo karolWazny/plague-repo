f2537b50a5fe445e90bba9152ffdaac8cfb3ac36
https://github.com/kuba342 <j.derda010@gmail.com>
2020-05-06 17:25:59 +0200

    Po mergu

--- projekt-manhattan/src/map/Hospital.java
+++ projekt-manhattan/src/map/Hospital.java
@@ -1,16 +1,9 @@
 package map;
 
 public class Hospital extends Building {
-<<<<<<< HEAD
-
-    //Pola
-    private static int hospitalCounter = 0;
-
-=======
     private static int hospitalCounter = 0;
 
     ////////////////////////////
->>>>>>> d8a5de7b4aa593db16f7eaff2f43881aa1e3f02a
     //Konstruktor
     public Hospital(){
         super("Hospital", 'H', 100);
@@ -20,14 +13,6 @@ public class Hospital extends Building {
     public Hospital(String id, char representation, int capacity){
         super(id, representation, capacity);
         hospitalCounter++;
-<<<<<<< HEAD
-    }
-
-    //Metody
-    public int getHospitalCounter(){
-        return hospitalCounter;
-=======
->>>>>>> d8a5de7b4aa593db16f7eaff2f43881aa1e3f02a
     }
 
     ////////////////////////////
