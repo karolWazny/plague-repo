c82ed4e6671749cc76c18b4c41d5a98d7a03eff8
Karol Wazny <karoljanwazny@gmail.com>
2020-05-07 11:50:45 +0200

    usunięcie starej metody z IRecord

--- projekt-manhattan/src/map/IRecord.java
+++ projekt-manhattan/src/map/IRecord.java
@@ -3,8 +3,6 @@ package map;
 public interface IRecord {
     public void move();
     public Coordinates getVerHor();
-    @Deprecated
-    public void setVerHor(int[] newVerHor);//Czy ta funkcja jest zdefiniowana?
     public void setVerHor(Coordinates currentVerHor);
     public void infectNeighbours(Map map);
     public void progressIllness();
