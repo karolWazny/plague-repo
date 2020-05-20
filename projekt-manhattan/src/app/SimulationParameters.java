f6fab25850b9b25a3c6e16b6e7faa2ca0ca7b81f
https://github.com/kuba342 <j.derda010@gmail.com>
2020-05-20 20:17:48 +0200

    Konstruktory

--- projekt-manhattan/src/app/SimulationParameters.java
+++ projekt-manhattan/src/app/SimulationParameters.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.io.File;
+
 public class SimulationParameters {
     int mapLength;
     int mapWidth;
@@ -18,17 +20,36 @@ public class SimulationParameters {
 
     SimulationParameters(int mapLength, int mapWidth, int numPeople, int numDocs, int numAmbulance, int numHearse, int power1,
     int power2, int timeTilInfect, int timeTilCured, int infectionRate, int activeRate) {
-    this.mapLength = mapLength;
-    this.mapWidth = mapWidth;
-    this.numPeople = numPeople;
-    this.numDocs = numDocs;
-    this.numAmbulance = numAmbulance;
-    this.numHearse = numHearse;
-    this.power1 = power1;
-    this.power2 = power2;
-    this.timeTilInfect = timeTilInfect;
-    this.timeTilCured = timeTilCured;
-    this.infectionRate = infectionRate;
-    this.activeRate = activeRate;
+        this.mapLength = mapLength;
+        this.mapWidth = mapWidth;
+        this.numPeople = numPeople;
+        this.numDocs = numDocs;
+        this.numAmbulance = numAmbulance;
+        this.numHearse = numHearse;
+        this.power1 = power1;
+        this.power2 = power2;
+        this.timeTilInfect = timeTilInfect;
+        this.timeTilCured = timeTilCured;
+        this.infectionRate = infectionRate;
+        this.activeRate = activeRate;
+    }
+
+    SimulationParameters(){
+        this.mapLength = 50;
+        this.mapWidth = 50;
+        this.numPeople = 10;
+        this.numDocs = 5;
+        this.numAmbulance = 2;
+        this.numHearse = 2;
+        this.power1 = 10;
+        this.power2 = 10;
+        this.timeTilInfect = 5;
+        this.timeTilCured = 10;
+        this.infectionRate = 10;
+        this.activeRate = 10;
+    }
+
+    SimulationParameters(File plik){
+        
     }
 }
