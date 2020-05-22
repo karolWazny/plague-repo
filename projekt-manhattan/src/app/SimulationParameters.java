c90c481b21368f92e9afeebf72df90a622548e95
https://github.com/kuba342 <j.derda010@gmail.com>
2020-05-22 10:18:54 +0200

    Poprawki w odczycie pliku

--- projekt-manhattan/src/app/SimulationParameters.java
+++ projekt-manhattan/src/app/SimulationParameters.java
@@ -60,50 +60,57 @@ public class SimulationParameters {
             String line = in.nextLine();
 
             while(in.hasNextLine()){
-                int index1 = line.indexOf("=");
-
-                String name = line.substring(0, index1);
-          
-                String number = line.substring(index1+1);
-
-                if(name.equals("mapLength")){
-                    this.mapLength = Integer.valueOf(number);
-                }
-                else if(name.equals("mapWidth")){
-                    this.mapWidth = Integer.valueOf(number);
-                }
-                else if(name.equals("numPeople")){
-                    this.numPeople = Integer.valueOf(number);
-                }
-                else if(name.equals("numDocs")){
-                    this.numDocs = Integer.valueOf(number);
-                }
-                else if(name.equals("numAmbulance")){
-                    this.numAmbulance = Integer.valueOf(number);
-                }
-                else if(name.equals("numHearse")){
-                    this.numHearse = Integer.valueOf(number);
-                }
-                else if(name.equals("power1")){
-                    this.power1 = Integer.valueOf(number);
-                }
-                else if(name.equals("power2")){
-                    this.power2 = Integer.valueOf(number);
-                }
-                else if(name.equals("timeTilInfect")){
-                    this.timeTilInfect = Integer.valueOf(number);
-                }
-                else if(name.equals("timeTilCured")){
-                    this.timeTilCured = Integer.valueOf(number);
-                }
-                else if(name.equals("infectionRate")){
-                    this.infectionRate = Integer.valueOf(number);
-                }
-                else if(name.equals("activeRate")){
-                    this.activeRate = Integer.valueOf(number);
-                }
                 
-                line = in.nextLine();
+                int index1 = line.indexOf("=");
+                
+                if(index1 != 0){
+                    String name = line.substring(0, index1);
+          
+                    String number = line.substring(index1+1);
+
+                    if(name.equals("mapLength")){
+                        this.mapLength = Integer.valueOf(number);
+                    }
+                    else if(name.equals("mapWidth")){
+                        this.mapWidth = Integer.valueOf(number);
+                    }
+                    else if(name.equals("numPeople")){
+                        this.numPeople = Integer.valueOf(number);
+                    }
+                    else if(name.equals("numDocs")){
+                        this.numDocs = Integer.valueOf(number);
+                    }
+                    else if(name.equals("numAmbulance")){
+                        this.numAmbulance = Integer.valueOf(number);
+                    }
+                    else if(name.equals("numHearse")){
+                        this.numHearse = Integer.valueOf(number);
+                    }
+                    else if(name.equals("power1")){
+                        this.power1 = Integer.valueOf(number);
+                    }
+                    else if(name.equals("power2")){
+                        this.power2 = Integer.valueOf(number);
+                    }
+                    else if(name.equals("timeTilInfect")){
+                        this.timeTilInfect = Integer.valueOf(number);
+                    }
+                    else if(name.equals("timeTilCured")){
+                        this.timeTilCured = Integer.valueOf(number);
+                    }
+                    else if(name.equals("infectionRate")){
+                        this.infectionRate = Integer.valueOf(number);
+                    }
+                    else if(name.equals("activeRate")){
+                        this.activeRate = Integer.valueOf(number);
+                    }
+                
+                    line = in.nextLine();
+                   
+                    }
+                    else{
+                        line = in.nextLine();
+                    }
             }
             in.close();
         }
