3d2e6cc1751a371531029b8c334878719a375dd3
Karol Wazny <karoljanwazny@gmail.com>
2020-05-27 09:58:17 +0200

    usunięcie testowego odczytu

--- projekt-manhattan/src/app/App.java
+++ projekt-manhattan/src/app/App.java
@@ -9,11 +9,6 @@ public class App {
         File plik = new File("D:/programowanieProjekty/sluzbowe/naStudia/plague-repo/projekt-manhattan/src/app/Config.txt");
         
         SimulationParameters parametry = new SimulationParameters();
-        System.out.println("mapLength = " + parametry.mapLength);
-        
-        parametry.ReadFromFile(plik);
-
-        System.out.println("After ReadFromFile(); mapLength = " + parametry.mapLength);
         Simulation sim2 = new Simulation(parametry);
         sim2.doSimulation();
     }
