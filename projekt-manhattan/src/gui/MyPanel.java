5064d4d9e3e14028062e9c09aace873ef6522998
Karol Wazny <karoljanwazny@gmail.com>
2020-06-01 08:57:46 +0200

    wyświetlanie obrazka

--- projekt-manhattan/src/gui/MyPanel.java
+++ projekt-manhattan/src/gui/MyPanel.java
@@ -1,8 +1,13 @@
 package gui;
 
 import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class MyPanel extends JPanel {
 
@@ -16,5 +21,18 @@ public class MyPanel extends JPanel {
         super();
         
         File plik = new File("cute_cat.jpg");
+        try {
+            obraz = ImageIO.read(plik);
+        } catch (IOException e) {
+            System.err.println("Blad odczytu obrazka");
+            e.printStackTrace();
+        }
+        setPreferredSize(new Dimension(obraz.getWidth(),obraz.getHeight()));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.drawImage(obraz,0,0,this);
     }
 }
