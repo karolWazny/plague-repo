5b3bda67e2ff5a0f65775c05822c4cd981d586a0
https://github.com/kuba342 <j.derda010@gmail.com>
2020-06-05 11:16:14 +0200

    komentarze do pakietu Map

--- projekt-manhattan/src/map/Being.java
+++ projekt-manhattan/src/map/Being.java
@@ -1,32 +1,55 @@
 package map;
 
+/**
+ * Klasa, po której dziedziczy większość klas obiektów będących na mapie
+ * Abstrakcyjna klasa, która nie tworzy instancji
+ * @version 1.0
+ * @see map.IPrintable
+ */
 public abstract class Being implements IPrintable {
+    /**
+     * Pole - identyfikator bytu
+     */
     private final String id;
+    /**
+     * Reprezentacja znakowa (w konsoli)
+     */
     protected final Character representation;
 
-    ////////////////////////////
+    /**
+     * Metoda konstruktor klasy byt
+     * @param id ciąg znakowy - identyfikator
+     * @param representation znak, reprezentacja znakowa
+     */
     public Being(String id, char representation){
         this.id = id;
         this.representation = representation;
     }
 
-    ////////////////////////////
-
     @Override
+    /**
+     * Metoda zwracająca reprezentację znakową bytu
+     * @return reprezentacja znakowa
+     */
     public String toString(){
         return representation.toString();
     }
 
-    ////////////////////////////
-
-    ////////////////////////////
-
+    
     @Override
+    /**
+     * Metoda getter, która zwraca ciąg znakowy - identyfikator bytu
+     * @return ciąg znakowy identyfikatora
+     */
     public String getId() {
         return id;
     }
 
     @Override
+    /**
+     * Metoda getter, która zwraca reprezentację znakową bytu
+     * @return reprezentacja znakowa w konsoli
+     */
     public char getRepresentation(){
         return representation;
     }
--- projekt-manhattan/src/map/EmptySlot.java
+++ projekt-manhattan/src/map/EmptySlot.java
@@ -1,9 +1,16 @@
 package map;
 
+/**
+ * Klasa pustego pola na mapie
+ * Jego reprezentacją znakową jest spacja
+ * @version 1.0
+ * @see map.Being
+ */
 public final class EmptySlot extends Being {
-
-    ////////////////////////////
-
+    /**
+     * Metoda konstruktor pustego pola w konsoli, wykorzystuje konstruktor bytu
+     * @see map.Being
+     */
     public EmptySlot() {
         super("empty", ' ');
     }
