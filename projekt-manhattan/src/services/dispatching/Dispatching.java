565f135401d16f604d02b1d3284ab0fd9655d389
https://github.com/kuba342 <j.derda010@gmail.com>
2020-06-05 22:32:30 +0200

    Dispatching

--- projekt-manhattan/src/services/dispatching/Dispatching.java
+++ projekt-manhattan/src/services/dispatching/Dispatching.java
@@ -10,29 +10,57 @@ import services.vehicles.Ambulance;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * Klasa, która jest zarządzającym obiektem dla wszystkich karetek
+ * Posiada informacje o karetkach i pacjentach
+ * @version 1.0
+ * @see container.IRecord
+ * @see map.Being
+ * @see services.vehicles.Ambulance 
+ * @see human.IDiseaseSensitive 
+ */
 public class Dispatching extends Being{
-
+    /** Lista rekordów dla ludzi, którzy potrzebują pomocy */
     private List<IRecord> callerList;
+    /** Lista rekordów z karetkami */
     private List<Ambulance> ambulanceList;
 
+    /**
+     * Metoda, konstruktor domyślny, która tworzy obiekt klasy Dispatching
+     */
     public Dispatching(){
         super("disp", 'i');
         callerList = new LinkedList<IRecord>();
         ambulanceList = new LinkedList<>();
     }
 
+    /**
+     * Metoda, getter, która zwraca listę obiektów oczekujących na pomoc
+     * @return lista obiektów oczekujących na karetkę
+     */
     public List<IRecord> getCallerList(){
         return callerList;
     }
 
+    /**
+     * Metoda, która dodaje nowe rekordy do listy potrzebujących 
+     * @param record nowy potrzebujący pomocy
+     */
     public void addCaller(IRecord record){
         callerList.add(record);
     }
 
+    /**
+     * Metoda, która dodaje nowy rekord ambulansu do listy ambulansów
+     * @param ambulance karetka
+     */
     public void addAmbulance(Ambulance ambulance){
         ambulanceList.add(ambulance);
     }
 
+    /**
+     * Metoda, która dysponuje karetkami do potrzebujących
+     */
     public void doTheJob(){
         Iterator <IRecord> iterator= callerList.iterator();
         while(iterator.hasNext()){
@@ -52,6 +80,10 @@ public class Dispatching extends Being{
         }
     }
 
+    /**
+     * Metoda, getter, która pobiera obydwie listy naraz 
+     * @return łączona lista wszystkich obiektów
+     */
     public List<IRecord> getBothLists(){
         List<IRecord> output = new LinkedList<>();
         output.addAll(callerList);
