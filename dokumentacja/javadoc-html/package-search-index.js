178bb5fce46c9c6b39859dc543e97307b5fea29c
Karol Wazny <karoljanwazny@gmail.com>
2020-06-12 14:24:55 +0200

    javadoc-html

---
+++ dokumentacja/javadoc-html/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"app"},{"l":"container"},{"l":"gui.frames"},{"l":"gui.panels"},{"l":"human"},{"l":"map"},{"l":"nasza_druzyna.projekt"},{"l":"random"},{"l":"services.buildings"},{"l":"services.dispatching"},{"l":"services.vehicles"},{"l":"virus"}]
